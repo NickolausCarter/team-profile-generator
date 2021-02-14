9472fd9d3686c85ccb28ede43521fe9f46c7dd59
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-02-14 12:53:19 -0600

    changed code for input to app

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -12,7 +12,7 @@ test('creates an engineer object', () => {
 test("gets engineer's github", () => {
   const engineer = new Engineer('Nick', 1, 'nickolaus.carter81@gmail.com', 'Engineer', 'nickolauscarter');
   
-  expect(engineer.getGithub()).toEqual(expect.stringContaining('https://github.com/nickolauscarter'));
+  expect(engineer.getGithub()).toEqual(expect.stringContaining('nickolauscarter'));
 });
 
 test("gets engineer's role", () => {
